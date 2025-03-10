da58856ee605593c66b5f35fc695475dac8d7fe4
Angelika Brown <angelikpbrown@gmail.com>
2025-03-09 22:21:26 -0400

    Triangles pasted in. Only need to remove the souts in Table Utilties so it can return

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,20 +3,38 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < numberOfStars; i++) {
+            sb.append('*');
+        }
+        return sb.toString();
+        //return null;
     }
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 1; i < numberOfRows; i++) {
+            sb.append(getRow(i)).append("\n");
+        }
+        return sb.toString();
+
+
+        //return null;
     }
 
     // hmm
-    
+
     public static String getSmallTriangle() {
-        return null;
+        return getTriangle(5);
+        //return null;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(10);
+        //return null;
     }
 }
