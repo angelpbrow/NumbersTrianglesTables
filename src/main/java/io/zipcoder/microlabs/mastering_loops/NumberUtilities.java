8de29299ad9f1e7f50b331fe32135d12ad73f7be
Angelika Brown <angelikpbrown@gmail.com>
2025-03-09 22:17:00 -0400

    Working on the wrong .git file this entire time

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -4,38 +4,80 @@ package io.zipcoder.microlabs.mastering_loops;
 public class NumberUtilities {
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        //return null;
+
+        StringBuilder sb = new StringBuilder();
+        while (start < stop) {
+            sb.append((int) Math.pow(start, exponent));
+            start = start + step;
+
+        }
+        return sb.toString();
+
     }
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        //return null;
+        StringBuilder sb = new StringBuilder();
+        while (start < stop) {
+            sb.append(start);
+            start = start + step;
+        }
+        return sb.toString();
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        return getRange(start, stop, 1);
     }
 
     public static String getRange(int stop) {
-        return null;
+        return getRange(0, stop, 1);
     }
 
     /*
      * natural break
      */
 
-    public static boolean isNumberEven(int toTest) { return false; }
-    public static boolean isNumberOdd(int toTest) { return false; }
-
-    public static String getEvenNumbers(int start, int stop) {
-        return null;
+    public static boolean isNumberEven(int toTest) {
+        //return false;
+        //
+        return (toTest % 2) == 0;
     }
 
-    public static String getOddNumbers(int start, int stop) {
-        return null;
+    public static boolean isNumberOdd(int toTest) {
+        return (toTest % 2 == 1);
     }
 
-    public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+    public static String getEvenNumbers ( int start, int stop){
+        //return null;
+        StringBuilder sb = new StringBuilder(); //what
+        while (start < stop) {
+            if(isNumberEven(start)) {
+                sb.append(start);
+            }
+            start = start + 1;
+
+        }
+        return sb.toString();
+    }
+
+    public static String getOddNumbers ( int start, int stop) {
+        StringBuilder sb = new StringBuilder();
+        while (start < stop) {
+            if(isNumberOdd(start)) {
+                sb.append(start);
+            }
+            start = start + 1;
+
+
+        }
+        return sb.toString();
+    }
+
+    public static String getSquareNumbers ( int start, int stop, int step){
+        //use the method you already made -- get Exponents except step by 2.
+        return NumberUtilities.getExponentiations(start, stop, step, 2);
     }
 
 }
+
