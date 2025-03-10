057c6fc9d6a2f81de4245c09e6db3c1ef9200437
Angelika Brown <angelikpbrown@gmail.com>
2025-03-09 22:19:28 -0400

    Table Utilities pasted in

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,15 +1,33 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
+
+    public static void printRow(int n, int cols) {
+        int i = 1;
+        while (i <= cols) {
+            System.out.printf("%4d", n * i);
+            i = i + 1;
+        }
+        System.out.println();
+    }
+
     public static String getMultiplicationTable(int tableSize) {
+
+        int i = 1;
+        while (i <= tableSize) {
+            printRow(i, tableSize);
+            i = i +1;
+        }
         return null;
     }
 
     public static String getSmallMultiplicationTable() {
-        return null;
+
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+
+        return getMultiplicationTable(10);
     }
 }
